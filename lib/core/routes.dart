17984ba597a1e4b1e0a21633b2f67eb57f9befd4
morpa <andremorpa@gmail.com>
2023-11-19 08:25:10 +0000

    initial structure

---
+++ lib/core/routes.dart
@@ -0,0 +1,29 @@
+import 'package:go_router/go_router.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../auth/auth.dart';
+import '../memory/memory.dart';
+import '../onboarding/onboarding.dart';
+
+part 'routes.g.dart';
+
+@riverpod
+GoRouter route(RouteRef _) => _routes;
+
+final _routes = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const MemoryPage(),
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginPage(),
+    ),
+    GoRoute(
+      path: '/verification',
+      builder: (context, state) => const VerificationPage(),
+    ),
+  ],
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'core/routes.dart';
+
+Future<void> main() async {
+  await Supabase.initialize(
+    url: '',
+    anonKey: '',
+  );
+
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final routes = ref.read(routeProvider);
+    return MaterialApp.router(
+      title: 'Memories App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
       ),
+      routerConfig: routes,
     );
   }
 }
